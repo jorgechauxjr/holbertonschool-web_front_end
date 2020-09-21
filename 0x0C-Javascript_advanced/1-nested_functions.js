af60a6f1389dec4bf67cd0d129f212af752c15de
jorgechauxjr <chauxjorge@hotmail.com>
2020-09-21 09:40:50 -0500

    Add 1-nested_functions.js

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,21 @@
+globalVariable = "Welcome";
+
+function outer() {
+    alert(globalVariable);
+    let course = "Holberton";
+
+    function inner(){
+        alert(globalVariable + " " + course)
+        let exclamation = "!";
+
+        function inception(){
+            alert(globalVariable + " " + course + exclamation);
+            inception();
+        }
+
+        inception();
+    }
+    inner();
+}
+
+outer();
