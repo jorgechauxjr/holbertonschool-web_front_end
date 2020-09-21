f9c7ad7bf04efb6c507a6abaaa975baf5218890a
jorgechauxjr <chauxjorge@hotmail.com>
2020-09-20 19:28:05 -0500

    Add 0-welcome.js

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+function welcome(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+    
+    function displayFullName(){
+        alert("welcome " + fullName + "!");
+    }
+    displayFullName();
+}
